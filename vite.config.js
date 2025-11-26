7ae0abffe6c247ab3b495edf57ce1b72ad6296fd
Albaperez2 <degnanrogerphilippe@gmail.com>
2025-11-26 15:44:22 +0100

    Final Fix: Switched Vite base path to relative ('./') for Netlify compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/' // Indique que les chemins des assets doivent partir de la racine du domaine
+  base: ./ // Indique que les chemins des assets doivent partir de la racine du domaine
 })
